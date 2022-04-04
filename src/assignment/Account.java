8c941af5cb67cf88bd77bd0c25e92476f0399480
dudutaelena19 <elenaduduta352@gmail.com>
2022-04-04 15:21:27 +0300

    Meaningful attribute names

---
+++ src/assignment/Account.java
@@ -0,0 +1,68 @@
+package assignment;
+
+public class Account {
+	private double	loanValue;
+	private double rate;
+	private int	daysActive;
+	private AccountType accountType;
+	
+	public double getLoanValue() {
+		System.out.println("The loan value is " + this.loanValue);
+		return loanValue;
+	}
+	
+	public double getRate() {
+		System.out.println("The rate is "+rate);
+		return this.rate;
+	}
+	
+	//must have method - the lead has requested it in all classes
+	public double getMonthlyRate() {
+		return loanValue*rate;
+	}
+	
+	public void setValue(double value) throws Exception {
+		if(value<0)
+			throw new Exception();
+		else
+		{
+			loanValue = value;
+		}
+	}
+	
+	public String to_string() {
+		return "Loan: "+this.loanValue+"; rate: "+this.rate+"; days active:"+daysActive+"; Type: "+accountType+";";
+	}
+	
+	public void print() {
+		int vb = 10;
+		System.out.println("This is an account");
+	}
+
+	public static double calculate(Account[] 	accounts)
+	{
+	double totalFee=0.0;
+	Account	account;
+	int temp = 365;
+	for	(int	i=0;i<accounts.length;i++)	{
+	account=accounts[i];
+	if(account.accountType==AccountType.PREMIUM||account.accountType==AccountType.SUPER_PREMIUM)	
+	totalFee+=.0125	*	(	//	1.25%	broker's	fee
+			account.loanValue*Math.pow(account.rate,(account.daysActive/365)) - account.loanValue);	//	interest-principal
+	}
+	return	totalFee;
+	}
+
+	public Account(double value, double rate, int account_Type) throws Exception {
+		if(value<0)
+			throw new Exception();
+		else
+		{
+			loanValue = value;
+		}
+		this.rate = rate;
+		this.accountType = accountType;
+	}
+	
+	
+}
---
+++ src/assignment/AccountType.java
@@ -0,0 +1,5 @@
+package assignment;
+
+public enum AccountType {
+	STANDARD,BUDGET,PREMIUM,SUPER_PREMIUM
+}
